0073c6e1ebc30fba329c21df8235c7c9ad9441e7
fahd <haouarifahd1@gmail.com>
2019-11-25 10:58:43 +0000

    f

---
+++ transfertfichier/src/transfertfichier/traitement.java
@@ -0,0 +1,31 @@
+package transfertfichier;
+import java.net.*;
+import java.io.*;
+
+public class traitement extends Thread{
+
+
+ Socket sock;
+	  BufferedReader entree;
+
+	  public traitement(Socket socket)
+	  { sock=socket;
+	    try
+	    { entree=new BufferedReader(new InputStreamReader(sock.getInputStream()));
+	    }
+	    catch(IOException e) { }
+	    this.start();
+	  }
+
+	  public void run()
+	  { String text;
+	    try
+	    { text=entree.readLine();
+	      System.out.println(text);
+	      sock.close();
+	    }
+	    catch(IOException e) { }
+	  }
+	}
+
+
